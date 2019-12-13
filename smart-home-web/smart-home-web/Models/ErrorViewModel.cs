823135f1cb63ccf3f9172d47cb05b9e84fdeb65f
RomanLytvyn <RomanOlexLytvyn@gmail.com>
2019-12-13 05:41:33 -0800

    Created initial app.

---
+++ smart-home-web/smart-home-web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace smart_home_web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
