f47e5fd4b6adb2031a1eeb52b465e90bd73822ee
Dan Nechypor <nechypor.dan@gmail.com>
2020-01-02 20:52:07 +0200

    create parser

---
+++ Infrastructure.Business/Infrastructure/ValueParser.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Business.Infrastructure
+{
+    class ValueParser 
+    {
+        public ValuesModel Values;
+
+        public ValuesModel Parse (string value)
+        {
+            if (int.TryParse(value, out Values.IntValue))
+                return Values;
+            if (double.TryParse(value, out Values.DoubleValue))
+                return Values;
+            if (bool.TryParse(value, out Values.BoolValue))
+                return Values;
+
+            Values.StringValue = value;
+           
+            return Values;
+        }
+    }
+}
