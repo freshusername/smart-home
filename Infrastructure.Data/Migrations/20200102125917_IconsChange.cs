bc1517836cecb382d52cf11f70ff66adf0d86ae8
Zahreichuk <email.andie.one@gmail.com>
2020-01-02 15:46:01 +0200

    - generate unique names for icons - delete Name prop from Icon

---
+++ Infrastructure.Data/Migrations/20200102125917_IconsChange.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infrastructure.Data.Migrations
+{
+    public partial class IconsChange : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Icons");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Icons",
+                nullable: true);
+        }
+    }
+}
