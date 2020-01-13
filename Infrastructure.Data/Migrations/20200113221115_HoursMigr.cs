31866207f4c065318866f76a4dc3689471850eda
Stasia <stasencia@gmail.com>
2020-01-14 00:23:04 +0200

    #20 add hours property

---
+++ Infrastructure.Data/Migrations/20200113221115_HoursMigr.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infrastructure.Data.Migrations
+{
+    public partial class HoursMigr : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Type",
+                table: "ReportElements",
+                nullable: false,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AddColumn<int>(
+                name: "Hours",
+                table: "ReportElements",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Hours",
+                table: "ReportElements");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Type",
+                table: "ReportElements",
+                nullable: false,
+                oldClrType: typeof(string));
+        }
+    }
+}
