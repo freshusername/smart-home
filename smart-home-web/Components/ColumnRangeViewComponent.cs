76a1aa9d36e15abbaea173cc684a91944823e332
Vadim Khariton <sg2391836@gmail.com>
2020-01-13 14:20:36 +0200

    created necessary files and folders for columnrange

---
+++ smart-home-web/Components/ColumnRangeViewComponent.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace smart_home_web.Components
+{
+    public class ColumnRangeViewComponent : BaseViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
