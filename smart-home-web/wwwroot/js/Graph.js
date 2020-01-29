75a5d85c93e285bd921f85cb700e94da8df53d7e
serhiybodnarchuk <serhiybodnarchuk@gmail.com>
2020-01-29 17:28:37 +0200

    #41 made real-time wordcloud

--- smart-home-web/wwwroot/js/Graph.js
+++ smart-home-web/wwwroot/js/Graph.js
@@ -3,6 +3,7 @@
 var connection = new signalR.HubConnectionBuilder().withUrl("/graphs").build();
 
 connection.on("UpdateGraph", function (sensorId, value, date) {
+    UpdateWordcloud(sensorId, value);
     UpdateGraph(sensorId, value, date);
 });
 
