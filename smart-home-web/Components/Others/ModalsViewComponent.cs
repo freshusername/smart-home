af4436ea0c5c7e7967b0aea2dbc2826bf6060220
Vadim Khariton <sg2391836@gmail.com>
2020-02-03 18:16:39 +0200

    move modals to component

---
+++ smart-home-web/Components/Others/ModalsViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace smart_home_web.Components.Others
+{
+    public class ModalsViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke() => View();
+    }
+}
