a4547747d9364bb5d069cb89f72cd386f3ac8d6e
serhiybodnarchuk <serhiybodnarchuk@gmail.com>
2020-02-03 15:27:18 +0200

    add migration "addSensorIdNull"

--- Infrastructure.Data/Migrations/20200203132638_addSensorIdNull.Designer.cs
+++ Infrastructure.Data/Migrations/20200203132638_addSensorIdNull.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Infrastructure.Data.Migrations
 {
     [DbContext(typeof(ApplicationsDbContext))]
-    partial class ApplicationsDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200203132638_addSensorIdNull")]
+    partial class addSensorIdNull
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
