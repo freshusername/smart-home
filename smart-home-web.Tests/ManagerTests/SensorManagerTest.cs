ea87fc5f986a5d9d8eb43886afdb4e9a5444eeaf
serhiybodnarchuk <serhiybodnarchuk@gmail.com>
2020-02-05 17:14:31 +0200

    wip

--- smart-home-web.Tests/ManagerTests/SensorManagerTest.cs
+++ smart-home-web.Tests/ManagerTests/SensorManagerTest.cs
@@ -1,4 +1,7 @@
-﻿using NUnit.Framework;
+﻿using Domain.Core.Model;
+using Infrastructure.Business.DTOs.Sensor;
+using Infrastructure.Business.Managers;
+using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -8,10 +11,31 @@ namespace smart_home_web.Tests.ManagerTests
     [TestFixture]
     class SensorManagerTest : TestInitializer
     {
+        private SensorManager _manager;
+        private SensorDto existingSensorDto;
+        private Sensor existingSensor;
+
         [SetUp]
         protected override void Initialize()
         {
             base.Initialize();
+            _manager = new SensorManager(mockUnitOfWork.Object, mockMapper.Object);
+        }
+
+        [Test]
+        public void Create_ValidDto_ReturnTrue()
+        {
+            Guid guid = Guid.NewGuid();
+            SensorDto newSensorDto = new SensorDto(){Name = "Correct",Token = guid, IsActive = true, IsValid=true};
+            Sensor newSensor = new Sensor();
+
+            mockMapper.Setup(m => m
+                .Map<Sensor>(newSensorDto))
+                    .Returns(newSensor);
+
+            var result = _manager.Create(newSensorDto);
+
+            Assert.IsTrue(result.Result.Succeeded);
         }
     }
 }
