ba37044d078c8f8117c78db4e20375b99da292bc
Vadim Khariton <sg2391836@gmail.com>
2020-02-07 16:36:47 +0200

    rewrite CRUD methods in some managers

--- Infrastructure.Business/Managers/SensorManager.cs
+++ Infrastructure.Business/Managers/SensorManager.cs
@@ -22,58 +22,48 @@ namespace Infrastructure.Business.Interfaces
 
         }
 
-        public async Task<OperationDetails> Create(SensorDto sensorDto)
-        {
-            try
-            {
-                Sensor sensor = mapper.Map<SensorDto, Sensor>(sensorDto);
-                try
-                {
-                    await unitOfWork.SensorRepo.Insert(sensor);
-
-                    unitOfWork.Save();
-                }
-                catch (Exception ex)
-                {
-                    return new OperationDetails(false, ex.Message, "Error");
-                }
-
-            }
-            catch (Exception ex)
-            {
-                return new OperationDetails(false, ex.Message, "Error");
-            }
-            return new OperationDetails(true, "New sensor has been added", "Name");
-        }
-
-        public OperationDetails Update(SensorDto sensorDto)
+        public async Task<SensorDto> Create(SensorDto sensorDto)
         {
             Sensor sensor = mapper.Map<SensorDto, Sensor>(sensorDto);
             try
             {
-                unitOfWork.SensorRepo.Update(sensor);
+                await unitOfWork.SensorRepo.Insert(sensor);
                 unitOfWork.Save();
             }
-            catch (Exception ex)
+            catch (Exception)
             {
-                return new OperationDetails(false, ex.Message, "Error");
+                return null;
             }
-            return new OperationDetails(true, "Sensor has been updated!", "Name");
+            return mapper.Map<Sensor, SensorDto>(sensor);
         }
 
-        public async Task<OperationDetails> Delete(int sensorId)
+        public async Task<SensorDto> Update(SensorDto sensorDto)
         {
-            Sensor sensor = await unitOfWork.SensorRepo.GetById(sensorId);
+            Sensor sensor = mapper.Map<SensorDto, Sensor>(sensorDto);
             try
             {
-                await unitOfWork.SensorRepo.Delete(sensor);
+                await unitOfWork.SensorRepo.Update(sensor);
+                unitOfWork.Save();
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+            return mapper.Map<Sensor, SensorDto>(sensor);
+        }
+
+        public async Task<OperationDetails> Delete(int id)
+        {
+            try
+            {
+                await unitOfWork.SensorRepo.DeleteById(id);
                 unitOfWork.Save();
             }
             catch (Exception ex)
             {
                 return new OperationDetails(false, ex.Message, "Error");
             }
-            return new OperationDetails(true, "Sensor has been deleted!", "Name");
+            return new OperationDetails(true, "Sensor has been deleted", "Name");
         }
 
         public async Task<SensorDto> GetSensorByIdAsync(int sensorId)
@@ -181,5 +171,7 @@ namespace Infrastructure.Business.Interfaces
             var sensor = await unitOfWork.SensorRepo.GetLastSensor();
             return mapper.Map<Sensor, SensorDto>(sensor);
         }
+
+        
     }
 }
