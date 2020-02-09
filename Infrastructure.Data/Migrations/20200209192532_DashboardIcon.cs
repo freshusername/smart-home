e333c0d47fd94a20b4216479287bca163a6fa3e1
Stasia <stasencia@gmail.com>
2020-02-09 21:50:25 +0200

    DashboardIcon migration

---
+++ Infrastructure.Data/Migrations/20200209192532_DashboardIcon.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infrastructure.Data.Migrations
+{
+    public partial class DashboardIcon : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "IconId",
+                table: "Dashboards",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Dashboards_IconId",
+                table: "Dashboards",
+                column: "IconId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Dashboards_Icons_IconId",
+                table: "Dashboards",
+                column: "IconId",
+                principalTable: "Icons",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Dashboards_Icons_IconId",
+                table: "Dashboards");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Dashboards_IconId",
+                table: "Dashboards");
+
+            migrationBuilder.DropColumn(
+                name: "IconId",
+                table: "Dashboards");
+        }
+    }
+}
